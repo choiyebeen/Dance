4bf8a7ad91fdc2b4188ce41339070db98149fff9
choiyebeen <dpqlsdl8@naver.com>
2023-12-03 16:25:20 +0900

    231203

--- Model/MainModel.cs
+++ Model/MainModel.cs
@@ -9,13 +9,6 @@ namespace Dance.Model
 {
     public class MainModel
     {
-        public ObservableCollection<string> NameList { get; set; }
-        public ObservableCollection<string> SingList { get; set; }
-        public MainModel()
-        {
-            //MainModel()을 객체화하면 자동으로 사용될 생성자
-            NameList = new ObservableCollection<string>();
-            SingList = new ObservableCollection<string>();
-        }
+
     }
 }
--- View/MainView.xaml.cs
+++ View/MainView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dance.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,20 @@ namespace Dance.View
         public MainView()
         {
             InitializeComponent();
+        }
+
+        private void Add_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = LeftListBox.SelectedItems.Cast<string>().ToList(); // LeftListBox.SelectedItems 가 비하인드 코드만 사용 가능
+            var viewModel = DataContext as MainViewModel;
+            viewModel?.LeftToRight(selectedItems); //LeftToRight가 뷰모델에 있어야 함
+        }
+
+        private void Remove_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = RightListBox.SelectedItems.Cast<string>().ToList();
+            var viewModel = DataContext as MainViewModel;
+            viewModel?.RightToLeft(selectedItems);
         }
     }
 }
